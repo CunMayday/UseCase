2e491fe2f860ab4b2c59bebdfa0a9e4243e56f36
CunMayday <caltinoz@purdueglobal.edu>
2025-09-18 10:15:02 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+// App.js
+import React from 'react';
+import './App.css';
+import StudentScoreAnalyzer from './components/StudentScoreAnalyzer';
+
+function App() {
+  return (
+    <div className="App">
+      <StudentScoreAnalyzer />
+    </div>
+  );
+}
+
+export default App;
